c228965d60b71c8b5238fa11dadaca4a9b3eeb25
Shodiq Muhammad <muhammadshodiq165@live.com>
2020-07-11 22:57:56 +0700

    add comments

--- src/main.rs
+++ src/main.rs
@@ -57,10 +57,16 @@ fn ownership() {
     println!("{}", got_referenced);
 }
 
+// this takes the param as reference and can be changed without making a return
+// this can only be done if the param is mutable 
 fn append_string(some_string: &mut String) {
     some_string.push_str(", world");
 }
 
+// this function creates a copy of s as the return
+// but since the s is created inside the no_dungle function
+// this will deallocated s inside the no_dungle function
+// so you have to copy it instead of accessing invalid pointer or cleaned memory
 fn no_dangle() -> String {
     let s = String::from("hello");
     s
